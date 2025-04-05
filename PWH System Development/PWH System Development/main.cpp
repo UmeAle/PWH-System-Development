13b36aa159ac09d74861cbdd64d686eb86e80fc9
Nicholas Reilly <nreilly4755@conestogac.on.ca>
2025-04-04 22:16:24 -0400

    Made the generic helper FileIO functions like open, close and get file path. Works for absolute paths only.
    Also made the specific load functions for the three seperate databases and one function that calls them all.

--- PWH System Development/PWH System Development/main.cpp
+++ PWH System Development/PWH System Development/main.cpp
@@ -4,6 +4,6 @@
 int main(void) {
 
 	//The program is going to be runned here
-
+	loadAllDatabases();
 	return 0;
 }
--- PWH System Development/PWH System Development/mainHeader.h
+++ PWH System Development/PWH System Development/mainHeader.h
@@ -1,10 +1,23 @@
 #pragma once
 
+//Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <windows.h>
 
 //Constants
-
+#define MAX_PATH_LEN 260
 
 //Structs
 
 
 //Prototypes
+errno_t openFile(FILE** file, const char* filename, const char* mode);
+void closeFile(FILE* file);
+char* getFilePath();
+void loadOrderDB();
+void loadPartsDB();
+void loadCustomerDB();
+void loadAllDatabases();
