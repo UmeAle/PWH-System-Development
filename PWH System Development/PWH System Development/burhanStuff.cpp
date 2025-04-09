b924c2421dfb444705457e5b0d6f7003d53ee2c3
Bshib36 <bshibli7194@conestogac.on.ca>
2025-04-09 17:14:31 -0400

    Update to listValidParts

--- PWH System Development/PWH System Development/burhanStuff.cpp
+++ PWH System Development/PWH System Development/burhanStuff.cpp
@@ -13,6 +13,7 @@ void listValidParts(const char* lineBuffer)
     int tokenCount = 0;
     char tempLine[300];
     char* context = NULL;
+    char* tokens[7]; 
 
     //Makes a copy of the line because strtok_s will change it.
     strncpy_s(tempLine, lineBuffer, sizeof(tempLine));
@@ -20,23 +21,29 @@ void listValidParts(const char* lineBuffer)
 
     //Breaks the line into pieces using '|'
     char* token = strtok_s(tempLine, "|", &context);
-    while (token != NULL) {
-        tokenCount++;
-
-        token = strtok_s(NULL, "|", &context);
+    while (token != NULL && tokenCount < 7) {
+        tokens[tokenCount++] = token;  
+        token = strtok_s(NULL, "|", &context); // Get the next token.
     }
 
     // Check if the record has exactly 7 fields so that it passes as valid, if not its invalid.
     if (tokenCount == 7) {
-        printf("Valid Part: %s\n", lineBuffer);
-
-        //log message
+        printf("\n");
+        printf("=============Valid Part=============\n");
+        printf("Part Name: %s\n", tokens[0]);      
+        printf("Part Number: %s\n", tokens[1]);    
+        printf("Part Location: %s\n", tokens[2]);  
+        printf("Part Cost: $%s\n", tokens[3]);      
+        printf("Quantity On Hand: %s\n", tokens[4]);
+        printf("Part Status: %s\n", tokens[5]);      
+        printf("Part ID: %s\n", tokens[6]);
+        printf("====================================\n");
+        
+       
         logEvent("INFO", "Part record processed successfully.");
     }
     else {
         printf("Invalid Part: %s (Token Count: %d)\n", lineBuffer, tokenCount);
-        
-        //log message
-        logEvent("WARNING", "Part record invalid - incorrect token count.");
+        logEvent("Error","Part record invalid");
     }
 }
